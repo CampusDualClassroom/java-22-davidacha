c91eaecf313282c7b22fd9abd585df0e039e10db
David Acha Oliveira <125604583+davidacha@users.noreply.github.com>
2024-10-01 10:03:38 +0200

    Ejercicio 22

--- src/main/java/com/campusdual/classroom/Exercise22.java
+++ src/main/java/com/campusdual/classroom/Exercise22.java
@@ -1,21 +1,42 @@
 package com.campusdual.classroom;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
 public class Exercise22 {
 
     public static List<String> createArrayList() {
-
+        List<String> a=new ArrayList<>();
+        String[] s={"GKFFD",
+                "TNANA",
+                "MPMSL",
+                "PSWME",
+                "LZMLF",
+                "JYEBV",
+                "YELNT",
+                "JSNKR",
+                "JFESF",
+                "TMJLL"};
+        a.addAll(Arrays.asList(s));
+        return a;
     }
 
     public static void printElementsAndIndex(List<String> customList) {
-
+        for(String s:customList){
+            System.out.println(s);
+        }
     }
 
     public static boolean addElementToList(List<String> customList, String element) {
-
+        int cont=customList.size();
+        customList.add(element);
+        if(cont<customList.size()){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     public static void main(String[] args) {
